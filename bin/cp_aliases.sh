b656cab187c737f361a952da075f7ba36b112f4c
awsomesawce <awsomesawce@outlook.com>
2020-12-11 21:35:49 -0800

    add cp_aliases script for easy copying from home directory

---
+++ bin/cp_aliases.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Script to copy the latest .bash_aliases file into ~/.dotfiles as ~/.dotfiles/ubuntu.bash_aliases
+cp ~/.bash_aliases ~/.dotfiles/ubuntu.bash_aliases
+cd ~/.dotfiles
+git add ubuntu.bash_aliases
+echo "added ubuntu.bash_aliases to git index"
+echo "use git commit to commit it."
