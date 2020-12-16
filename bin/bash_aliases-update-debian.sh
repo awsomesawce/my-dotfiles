e601e11e492e8a2a256d5ef49658dd6b1df269bb
Carl C <awsomesawce@outlook.com>
2020-12-16 00:00:47 -0800

    add micro installer and remove default commit msg in bash_aliases update script

--- bin/bash_aliases-update-debian.sh
+++ bin/bash_aliases-update-debian.sh
@@ -2,7 +2,7 @@
 # This script updates cp's current .bash_aliases file into dotfiles folder, adds, and does git commit.
 cp ~/.bash_aliases ~/.dotfiles/debian.bash_aliases # appends "debian" to file name
 cd ~/.dotfiles/
-git add ~/.dotfiles/debian.bash_aliases && git commit -m 'update debian.bash_aliases'
+git add ~/.dotfiles/debian.bash_aliases && git commit 
 echo "Added and commited .bash_aliases to ~/.dotfiles/ git repo"
-echo "Use git push to push to remote"
+echo "Use git push to push to remote."
 
