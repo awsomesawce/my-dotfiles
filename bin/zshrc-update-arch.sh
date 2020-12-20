0698ce1da799c177509a854f061ce749a19b443d
Carl C <awsomesawce@outlook.com>
2020-12-19 22:21:11 -0800

    add script for zshrc update in ARCH linux

---
+++ bin/zshrc-update-arch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This script updates cp's current .zshrc file into dotfiles folder, adds, and does git commit.
+cp ~/.zshrc ~/.dotfiles/arch.zshrc
+cd ~/.dotfiles/
+git add ~/.dotfiles/arch.zshrc && git commit
+echo "copied .zshrc to ~/.dotfiles git repo as arch.zshrc."
+echo "Use git push to push to server!"
+echo "Have a great day!"
+
