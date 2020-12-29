8b85b551aa8d59a4bab23ae468a1b9859a1e01ca
Carl C <awsomesawce@outlook.com>
2020-12-29 14:56:26 -0800

    Add packages to arch.emacs, modify gaddc.sh

--- bin/gaddc.sh
+++ bin/gaddc.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # Simple script to combine git add . and git commit
-echo "This script will add all files to index and commits"
+echo "This script will add all modified files to index and commit the\n index"
 git add .
 git commit
 echo "Done!"
 echo "Thanks for using this script and have a great day!"
+
+return 0
