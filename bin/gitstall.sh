d80898d22604fbe0c70c5e9b9668a292d2254134
Carl C <awsomesawce@outlook.com>
2020-12-29 21:03:50 -0800

    Add directory information to echo portion of script.

--- bin/gitstall.sh
+++ bin/gitstall.sh
@@ -4,9 +4,9 @@ export DOTDIR=~/.dotfiles
 export WSLNOTES=/home/carlc/Documents/wsl-notes
 export NODESCHOOL=~/Documents/gitstuff/node-school
 echo "Here is your current status of $DOTDIR, $WSLNOTES, and $NODESCHOOL."
-cd $DOTDIR
+cd $DOTDIR && echo $DOTDIR
 git status
-cd $WSLNOTES
+cd $WSLNOTES && echo $WSLNOTES
 git status
-cd $NODESCHOOL
+cd $NODESCHOOL && echo $NODESCHOOL
 git status
