cd865b169cd6e6505d336d5c65e0dfc79c0b0328
Carl C <awsomesawce@outlook.com>
2021-01-29 02:18:45 -0500

    Add README for arch local home bin

--- bin/gitstall.sh
+++ bin/gitstall.sh
@@ -3,6 +3,7 @@
 export DOTDIR=~/.dotfiles
 export WSLNOTES=/home/carlc/Documents/wsl-notes
 export NODESCHOOL=~/Documents/gitstuff/node-school
+# TODO: add if statement to make sure the file or dir is there!
 echo "Here is your current status of $DOTDIR, $WSLNOTES, and $NODESCHOOL."
 cd $DOTDIR && echo $DOTDIR
 git status
@@ -10,3 +11,4 @@ cd $WSLNOTES && echo $WSLNOTES
 git status
 cd $NODESCHOOL && echo $NODESCHOOL
 git status
+exit 0
