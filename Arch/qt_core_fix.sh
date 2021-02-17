739bb71c000f9991750ed2f1b24c7f853323fd6a
Carl C <awsomesawce@outlook.com>
2021-02-16 22:37:38 -0500

    add warning to qt_core_fix.sh

--- Arch/qt_core_fix.sh
+++ Arch/qt_core_fix.sh
@@ -4,6 +4,9 @@
 # any program depending on these librarys will not work without this 
 # script.
 
+### TODO: This operation makes it so the file in question becomes a hard-link instead of a symbolic link.  This is a problem.
+### When upgrading, it might cause problems.
+
 if [ -x /usr/lib64/libQt5Core.so.5 ]; then
         (set -x; strip --remove-section=.note.ABI-tag /usr/lib64/libQt5Core.so.5)
 fi
