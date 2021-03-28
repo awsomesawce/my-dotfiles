50818638cbcd20c02198fb33d5ee60001d32c849
Carl C <awsomesawce@outlook.com>
2021-03-28 17:29:50 -0400

    Update Powershell profile and config

--- windows/Microsoft.Powershell_profile.ps1
+++ windows/Microsoft.Powershell_profile.ps1
@@ -1,53 +1,81 @@
+# Disable Telemetry
+$env:POWERSHELL_TELEMETRY_OPTOUT = 1
+# Import current modules.
 Import-Module posh-git
 Import-Module oh-my-posh
+#Import-Module z
 # Set prompt
-Set-PoshPrompt -Theme powerlevel10k_classic
-# set psdir variable to local powershell directory, and set other variables.
+Set-PoshPrompt -Theme paradox && Write-Output "Set prompt to paradox"
+# Source other_functions script and projectvars script.
 $PSDirectory="D:/Carl/Documents/Powershell"
+$Script:scriptspwsh = "$env:USERPROFILE\gitstuff\scripts-pwsh\config"
+# Try ternary operation.
+$otherFunctionsScript = "$Script:scriptspwsh/other_functions.ps1"
+
+(Test-Path $otherFunctionsScript) ? (. $otherFunctionsScript) : (Write-Output "`$otherFunctionsScript not found here: $otherFunctionsScript")
+
+# if (Test-Path "$otherFunctionsScript") {
+#     . "$otherFunctionsScript"
+#     Write-Output "`$otherFunctionsScript has been loaded.  Its path = $otherFunctionsScript"
+# } else {
+#     "$otherFunctionsScript file not found."
+# }
+
+$projectvarsScript = "$Script:scriptspwsh\projectvars.ps1"
+(Test-Path $projectvarsScript) ? (. $projectvarsScript) : (Write-Output "projectvars.ps1 not found")
+# If ternary operator is incompatible, use this:
+# if (Test-Path ~/gitstuff/scripts-pwsh/config/projectvars.ps1) {
+# . ~/gitstuff/scripts-pwsh/config/projectvars.ps1
+# } else {
+# Write-Output "projectvars.ps1 not found"
+# }
+# set psdir variable to local powershell directory, and set other variables.
 $gitDir="D:/Carl/Documents/GitHub"
-$oneDrive="D:/Carl/OneDrive"
+$oneDrive="$env:OneDrive"
 $localAppData="C:/Users/Carl/AppData/Local"
 $globalAppData="D:/Carl/Appdata"
+# The environment variable below sets XDG_CONFIG_HOME, which then tells nvim to look for it's
+# init file in that directory instead of AppData/local/nvim
+# That is bad, we don't want that because it gets rid of our configuration.
+# If you want to start nvim without having any confing, set the $env:XDG_CONFIG_HOME variable.
+#$env:XDG_CONFIG_HOME="C:\Users\Carl\.config"
 
 ## This option sets the command line editor to have Emacs-like keybindings.
 ## You can set more options using Set-PSReadlineKeyHandler
-#Set-PSReadLineOption -EditMode Emacs
-$DOTFILESGIT = "$HOME/gitstuff/my-dotfiles"
+#Set-PSReadLineOption -EditMode Windows
+$DOTFILESGIT = "$env:USERPROFILE\gitstuff\my-dotfiles"
 function dotFiles {Set-Location $DOTFILESGIT}
 # Aliases
 # use single-quotes for strings with spaces
+# TODO: In progress: Moving Set-Alias declarations to separate
+# file. other_functions.ps1 in config dir.
 Function lsWide {Get-ChildItem | Format-Wide}
 Set-Alias -Name lsw -Value lsWide -Description 'ls but format-wide'
 Set-Alias -Name list -Value Get-ChildItemColorFormatWide
+Set-Alias -Name lsc -Value Get-ChildItemColorFormatWide
 
 ## Set-Location to a wsl distro file system
-Function gotoarch {Set-Location \\wsl$\Arch\home\carlc\}
-Function gotodebian {Set-Location \\wsl$\Debian\home\carlc\}
+Function gotoubuntu {Set-Location \\wsl$\Ubuntu-20.04\home\carlc}
 
 ## Open Powershell profile from anywhere
-Function psconfig {nvim $PSDirectory\Microsoft.Powershell_profile.ps1}
-Function editprofile {nvim $PROFILE}
-## Nvim config shortcut # remember to use backslashes.
-Function nvimconfig {nvim $localAppData\nvim\init.vim}
-Function neovconfig {neovide.ps1 $localAppData\nvim\init.vim}
+## Just type $EDITOR $PROFILE where $EDITOR is any text editor.
+# IMPORTANT: Copied nvim config functions to other_functions.ps1
 ## Nvim 2 window shortcut
 Function nvim2 {nvim -O2}
-Function nvim2 {nvim -O2}
-Function fvim2 {fvim -O2}
 Function nvim22 {nvim -O2 -p2}
-Function fvim22 {fvim -O2 -p2}
 Function nvimdiff {nvim -d}
-Function fvimdiff {fvim -d}
+function nvimqtdiff {nvim-qt -d $args}
 ## Shortcut for duckduckgo
 $duckduck = "https://duckduckgo.com"
-function ducks { Start-Process $duckduck }
-
-Set-Alias -Name np -Value C:\Windows\notepad.exe
+function ducks { Start-Process $duckduck/?q=$args }
 # This one sets an alias for the ri command in ruby
 set-alias -name rubyri -value D:\Ruby27-x64\bin\ri.cmd -Description "A workaround for ruby's ri, cuz in pwsh ri is remove-item"
-Set-Alias -Name exp -Value explorer
+Set-Alias -Name exp -Value explorer.exe
 Set-Alias -Name man -Value D:\Cygwin\bin\man.exe -Description "Show info file from cygwin which includes a lot more documentation than the windows emacs info"
-Set-Alias -Name lsc -Value Get-ChildItemColorFormatWide -Description "Better alias for color ls"
+#Set-Alias -Name lsc -Value Get-ChildItemColorFormatWide -Description "Better alias for color ls"
+# The above alias is already in your other_functions.ps1
+function linuxls {D:\Cygwin\ls.exe --color=always --group-directories-first}
+Set-Alias -Name lxls -Value linuxls -Description "Linux type of ls."
 Set-Alias -Name rdis -Value rdiscount -Description "Shorter rdiscount"
 Set-Alias -Name kram -Value kramdown -Description "Shorter kramdown"
 Set-Alias -Name pd -Value pandoc -Description "Shorter Pandoc"
@@ -55,12 +83,15 @@ Set-Alias -Name msys -Value D:\MSYS2\msys2_shell.cmd -Description "Use --help fo
 Set-Alias -Name git-cola -Value C:\Users\Carl\AppData\Local\git-cola\bin\git-cola.exe -Description "Git-cola because "
 ## Beware when starting shells directly from cygwin's bin.
 ## It will not inherit the necessary env variables from Windows.
-Set-Alias -Name cygbash -Value D:\Cygwin\bin\bash.exe -Description "Use man bash for help"
 Set-Alias -Name cygtcsh -Value D:\Cygwin\bin\tcsh.exe -Description "Cygwin's tcsh"
+$CYGDIR = (Get-Item "D:\Cygwin")
+Set-Variable -Name CYGBIN -Value "$CYGDIR\bin" -Description "Variable that points to Cygwin's bin directory."
+Set-Alias -Name cygbash -Value "$CYGBIN\bash.exe"
+## TODO: Move Aliases and Functions into their own respective files and source each file.
 ## Functions
 
 # Next is a function that allows me to change to my powershell directory where my profile is.
-Function GotoPSDir {Set-Location -Path PSDirectory}
+Function GotoPSDir {Set-Location -Path $PSDirectory}
 Set-Alias -Name psdir -Value GotoPSDir
 
 # This is just a function to cd to my notes dir
@@ -69,59 +100,44 @@ Function GotoNotesDir {Set-Location -Path $notesdir}
 # Set-Alias to make notes dir even more easily accessible
 Set-Alias -Name ndir -Value GotoNotesDir
 # Function for invoking ubuntu wsl with carlc user and zsh shell
-Function wslubuntu {wsl -d Ubuntu-20.04 -u carlc -e zsh}
-Function kak {wsl -d Arch -u carlc -e kak}
 # Function to get to standard parent git directory
 Function gitdir {Set-Location -Path $gitDir}
-Function nodeschool {Set-Location -Path -Verbose "D:\Carl\Documents\GitHub\D:\Carl\Documents\GitHub\node-school"}
+Function nodeschool {Set-Location -Path "D:\Carl\Documents\GitHub\node-school"}
 # get to emacs org directory located in OneDrive quickly.
 Function org_dir {Set-Location -Path $oneDrive\org_dir}
 # This is a function for git status, not Get-GitStatus, which is a posh-git cmdlet.
-Function gitst {git status}
 Function gpgmee {gpg -se -r Carl}
 Function localAppData {Set-Location -Path C:\Users\Carl\AppData\Local}
 Function chdirGAppData {Set-Location -Path $globalAppData}
 Function gcift {Get-ChildItem | Format-Table}
 Function npmDoc {Set-Location -Path 'C:\Program Files\nodejs\node_modules\npm\docs'}
 # Backup folder for dotfiles in both Windows and Ubuntu
-Function dotfiles_backup {Set-Location -Path $oneDrive\dotfiles_backup}
+Function gotodotfilesbackup {Set-Location -Path $oneDrive\dotfiles_backup}
 # Easily page thru long ls lists
 Function lspage {Get-ChildItem | less}
-Set-Alias -name lsless -Value lspage
 Set-Alias -Name lsl -Value lspage
-# Easily page thru get-help output
-Function helpless {Get-Help $1 | less}
-# Easily open powershell profile in fvim
-Function psProfileEdit {Invoke-Item D:\Carl\Documents\Powershell\Microsoft.Powershell_profile.ps1}
-Set-Alias -Name dotdir -Value dotfiles_backup
-Set-Alias -Name archl -Value 'D:/Arch/arch.exe'
-Set-Alias -Name lg -Value 'lazygit'
-Set-Alias -name localad -Value localappdata
+Set-Alias -Name dotdir -Value gotodotfilesbackup
+Set-Alias -Name lg -Value 'lazygit' -Description "lazygit is a git management cli"
+Set-Alias -name localad -Value localappdata 
 Set-Alias -Name gitbash -Value 'D:\Program Files\Git\git-bash.exe'
-Set-Alias -Name ... -Value D:\Carl\Documents\PowerShell\Scripts\backwards_cd.ps1
-Set-Alias -Name .. -Value D:\Carl\Documents\PowerShell\Scripts\backwards_cd1.ps1
+# TODO: Instead of creating external scripts, you can create a function for each.
+Set-Alias -Name ... -Value D:\Carl\Documents\PowerShell\Scripts\backwards_cd.ps1 -Description "go up two directories."
+Set-Alias -Name .. -Value D:\Carl\Documents\PowerShell\Scripts\backwards_cd1.ps1 -Description "go up a directory"
 Set-Alias -name ipinfo -Value D:\Carl\Documents\PowerShell\Scripts\getipinfo.ps1
 Set-alias -Name invo -Value Invoke-Item -description "Shorter invoke-item"
-#node C:\Users\Carl\node_modules\which\bin\node-which
 # Alias for Get-GitStatus function provided by posh-git
 set-alias -Name gitstat -Value Get-GitStatus
-Set-Alias -Name list -Value Get-ChildItemColor
-# Above makes the ls command always use Get-ChildItemColor
-# This attaches the rename command to rename item
 # ren is also an alias for rename-item
 Set-Alias -Name rename -Value Rename-Item -Description "A smart rename alias"
 Set-Variable -Name CYGBIN -Value 'D:/Cygwin/bin' -Description 'Location for cygwin binaries'
 Set-Variable -Name msysbin -Value D:\MSYS2\usr\bin
 Set-Alias -Name ghlp -Value Get-Help -Description "A shorter gethelp."
-Set-Alias -Name ghp -Value Get-Help -Description "Even shorter gethelp."
-Set-Alias -Name gethelp -Value Get-Help
-Set-Alias -Name ghelp -Value Get-Help
 # Reset rememberfile variable to quicktodo instead
 Set-Variable -Name quicktodo -Value D:\Carl\OneDrive\TODO\quicktodo.md
 Set-Variable -Name rememberfile -Value D:\Carl\OneDrive\TODO\quicktodo.md
 Set-Variable -Name remember -Value $oneDrive/remember.md
 Set-Variable -Name DESKTOP -Value D:\Carl\OneDrive\Desktop\ -Description "Shortcut to the Desktop folder"
-Write-Output "Welcome to Powershell!"
+Write-Output "Welcome Carl!"
 Set-Alias -Name shmd -Value Show-Markdown -Description "Alias for Show-Markdown"
 Function shmdall {
     Show-Markdown -Path *.md
@@ -129,27 +145,21 @@ Function shmdall {
 Function shmdbrowser {
     Show-Markdown -UseBrowser
 }
-Function chtsh {curl "https://cht.sh/$@"}  # This currently doesn't work.
 # TODO: Organize aliases and functions.
 # TODO: Put all aliases in separate script and source the script.
 # Hello from embedded nvim!
 # Use `K' to see a docstring for the cmdlet at point in `nvim'
-Set-Alias -Name l -Value Get-ChildItem -Description "Super small ls command"
 Set-Variable -Name NVIMINITVIM -Value C:\Users\Carl\AppData\Local\nvim\init.vim -Description "Main config file for neovim"
-Set-Alias -Name w3mducks -Value "w3m duckduckgo.com" -Description "w3m for ducks"
 Function dotgitdiff {Set-Location -Path C:\Users\Carl\gitstuff\my-dotfiles\ && git diff && cd -}
 Function dotgitstatus { Set-Location -Path "$HOME/gitstuff/my-dotfiles" && git status && cd - }
-Set-Alias -Name g -Value git -Description "Git in one letter"
 # Save this and other weird variables to a sourcable pwsh script:
-Set-Variable -Name randomoutput -Value D:\Carl\OneDrive\TODO\randomoutput.md
+Set-Variable -Name randomnotes -Value D:\Carl\OneDrive\TODO\randomoutput.md
 Set-Variable pwshsnippets -Value "D:\Carl\OneDrive\snippets\pwsh\powershell_snippets.txt" -Description "out-file for writing quick powershell snippets from the command line"
-Set-Alias -Name "sob" -Value "Select-Object" -Description "A shorter select"
-Set-Alias -Name add -Value Add-Content -Description "a shorter Add-content"
 
 ## Sourcing Scripts
 . D:\Carl\Documents\PowerShell\Scripts\_rg.ps1 # source rg completion script
-. $PSDirectory\Scripts\other_functions.ps1
-Function Node-Docs-Open {start https://nodejs.org/dist/latest-v14.x/docs/api/}
-Set-Alias -Name nodedoc -Value Node-Docs-Open -Description "Open NodeJS docs in a browser"
-TODO: Copy the above two lines to the other_functions.ps1 scriptfile, and set a variable to 
-refer to the script file for ease of access
+
+# DONE: Copy the above two lines to .\Scripts\other_functions.ps1 scriptfile, and set a variable to 
+# refer to the script file for ease of access
+# This expression is necessary for python's fuck module to work.
+#Invoke-Expression "$(thefuck --alias)"
---
+++ windows/projectvars.ps1
@@ -0,0 +1,43 @@
+# A list of variables that point to project directories
+# Use `gf` in vim to goto the file at point.
+# TODO: add second script for sourcing functions
+# Author: Carl C (awsomesawce@outlook.com)
+# Date: Mar 28, 2021
+
+# First, set the variable to this script.
+# TODO: Get this variable from $PSScriptRoot
+Set-Variable -Name projectVarsScript -Value "C:\Users\Carl\gitstuff\scripts-pwsh\config\projectvars.ps1" -Description "Variable pointing to the script."
+
+# Then, set the rest of the current project variables.
+set-variable -Name labscurrent -Value "~/Downloads/labs-node/labs-jan-5-2021/labs" -Description "Current labs"
+
+Set-Variable -Name "UserProfileD" -Value (Get-Item "D:\Carl") -Description "Userprofile directory on data drive"
+
+Set-Variable -Name GitDirectoryD -Value $UserProfileD\Documents\GitHub -Description "Main directory for github hosted projects"
+
+Set-Variable -Name nodeschool -Value "D:\Carl\Documents\GitHub\node-school\" -Description "node-school directory"
+
+Set-Variable -Name nodewebpackproj -Value "D:\Carl\Documents\GitHub\my-webpack-demo" -Description "My webpack demo git dir"
+
+Set-Variable -Name pwshsnippets -Value "D:\Carl\OneDrive\snippets\pwsh\powershell_snippets.txt" -Description "out-file for writing quick powershell snippets from the command line"
+
+Set-Variable -Name NVIMINITVIM -Value C:\Users\Carl\AppData\Local\nvim\init.vim -Description "Main config file for neovim"
+Set-Variable -Name DESKTOP -Value $env:OneDrive\Desktop\ -Description "Shortcut to the Desktop folder"
+Set-Variable -Name randomnotes -Value $env:OneDrive\TODO\randomoutput.md -Description "A place to pipe output from pwsh"
+
+function nodesch {set-location $nodeschool}
+
+$env:EDITOR = "nvim" # EDITOR variable to use when invoking unix-like programs.
+$env:PAGER = "less" # PAGER variable used by a ton of unix-like programs.
+Write-Output "Variables have been imported from $PSScriptRoot\projectvars.ps1"
+Set-Variable -Name cyghome -Value "D:\Cygwin\home\Carl" -Description "Set an env variable for this next time"
+
+($profile -clike "*Microsoft*") ? ("Default powershell profile " + $PROFILE + " loaded") : ("Powershell profile does not contain `"Microsoft`"")
+# Add variable for perlcbin installed from scoop
+$perlcbin = Get-item $env:USERPROFILE\scoop\apps\perl\current\c\bin\
+$binDirs = "$env:OneDrive\Desktop", "$env:USERPROFILE\bin", "D:\Carl\bin"
+function listBinDirs {
+  Write-Output "This will list all the bin directories and optionally their contents."
+  Write-Output "`$binDirs = $binDirs"
+  Write-Output "`$perlcbin = $perlcbin"
+}
