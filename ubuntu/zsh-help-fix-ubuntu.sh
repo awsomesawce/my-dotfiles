aa563ebd71db2a9806a93a3f6a1d862d09c08b70
awsomesawce <awsomesawce@outlook.com>
2021-05-02 02:24:28 -0400

    Fix zhelpfix function, add description to zsh-help-fix-ubuntu.sh

--- ubuntu/zsh-help-fix-ubuntu.sh
+++ ubuntu/zsh-help-fix-ubuntu.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env zsh
+#!/usr/bin/zsh -f
+# NOTE: the reason why this was so slow to load before was because it was not
+#       using the `-f` option, which meant `zsh` was loading all the startup files
+#       every time the script was executed.
+#       The `-f` option makes it so it does not load config files, it just executes the script.
+#
+# This script has been replaced by the zhelpfix function located in 
+# ~/.shell_functions and only loaded when sourced by zsh
 # Test for opening zsh's help from anywhere
 setopt nobeep
 setopt errexit
