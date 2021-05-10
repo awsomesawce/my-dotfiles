9831b0317461179398beaffad2721e555dd388af
Carl C <awsomesawce@outlook.com>
2021-05-09 20:01:08 -0400

    msys2 zsh_aliases, add other files.

--- bin/shell_learning/autorm3.sh
+++ bin/shell_learning/autorm3.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# script for automatically opening a README file
+# script for automatically opening a ./README file
 # no matter what extension
 
 if [ -x /usr/bin/bat ]; then
@@ -8,29 +8,29 @@ else
 	PAGER="${PAGER:=less}"
 fi
 
-if [ -r README ]; then
-    echo "Reading README"
-    "$PAGER" README
-elif [ -r README.md ]; then
-    echo "Reading README.md"
-    "$PAGER" README.md
-elif [ -r readme.md ]; then
-    echo "Reading readme.md"
-    "$PAGER" readme.md
-elif [ -r README.markdown ]; then
-    echo "Reading README.markdown"
-    "$PAGER" README.markdown
-elif [ -r README.mkd ]; then
-    echo "Reading README.mkd"
-    "$PAGER" README.mkd
-elif [ -r Readme.md ]; then
-    echo "Reading Readme.md"
-    "$PAGER" Readme.md
-elif [ -r README.txt ]; then
-    echo "Reading README.txt"
-    "$PAGER" README.txt
+if [ -r ./README ]; then
+    echo "Reading ./README"
+    "$PAGER" ./README
+elif [ -r ./README.md ]; then
+    echo "Reading ./README.md"
+    "$PAGER" ./README.md
+elif [ -r ./readme.md ]; then
+    echo "Reading ./readme.md"
+    "$PAGER" ./readme.md
+elif [ -r ./README.markdown ]; then
+    echo "Reading ./README.markdown"
+    "$PAGER" ./README.markdown
+elif [ -r ./README.mkd ]; then
+    echo "Reading ./README.mkd"
+    "$PAGER" ./README.mkd
+elif [ -r ./Readme.md ]; then
+    echo "Reading ./Readme.md"
+    "$PAGER" ./Readme.md
+elif [ -r ./README.txt ]; then
+    echo "Reading ./README.txt"
+    "$PAGER" ./README.txt
 else
-    echo "No readmes in directory"
+    echo "No ./readmes in directory"
 fi
 exit 0
 
