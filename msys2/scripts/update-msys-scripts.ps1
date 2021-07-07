01a3209433e4a62ea5b7447fbca9cc018ed0b214
Carl C <awsomesawce@outlook.com>
2021-07-06 21:23:48 -0400

    update update-msys-scripts script.

--- msys2/scripts/update-msys-scripts.ps1
+++ msys2/scripts/update-msys-scripts.ps1
@@ -1,3 +1,11 @@
-# Copys all msys2 scripts in ~/bin to this folder.
-$source = gci ~/bin/msys*, ~/bin/mbash*, ~/bin/pwsh*, ~/bin/mzsh*
+#!/usr/bin/env -S pwsh -noprofile
+<#
+.Description
+Copys all msys2 scripts in ~/bin to this folder.
+.NOTES
+Uses `/` unix style path separators instead of backslashes.
+Works just find in modern Powershell.
+#>
+
+$source = get-ChildItem ~/bin/msys*, ~/bin/mbash*, ~/bin/pwsh*, ~/bin/mzsh*
 copy-item $b ~/gitstuff/my-dotfiles/msys2/scripts
